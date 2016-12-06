616bb42c6c54778e23e8c65480464272f6bd36e2
minig <minig@LAPTOP-E3IFGU52>
2016-12-06 14:27:24 -0500

    Add project files.

---
+++ TeamProject2-Part2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TeamProject2_Part2.MvcApplication" Language="C#" %>
---
+++ TeamProject2-Part2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TeamProject2_Part2.Startup))]
+namespace TeamProject2_Part2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
