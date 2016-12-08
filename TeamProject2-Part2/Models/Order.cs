3586d557fd54ac600296f4a791a0cb1ae4e50db0
xuemeiwang <xwang231@my.centennialcollege.ca>
2016-12-08 00:19:40 -0500

    Update Order model & Register/Login page

--- TeamProject2-Part2/Models/Order.cs
+++ TeamProject2-Part2/Models/Order.cs
@@ -2,6 +2,7 @@ namespace TeamProject2_Part2.Models
 {
     using System;
     using System.Collections.Generic;
+    using System.ComponentModel;
     using System.ComponentModel.DataAnnotations;
     using System.ComponentModel.DataAnnotations.Schema;
     using System.Data.Entity.Spatial;
@@ -15,40 +16,59 @@ namespace TeamProject2_Part2.Models
             OrderDetails = new HashSet<OrderDetail>();
         }
 
+        [ScaffoldColumn(false)]
         public int OrderId { get; set; }
 
+        [ScaffoldColumn(false)]
         public DateTime OrderDate { get; set; }
 
+        [ScaffoldColumn(false)]
         [StringLength(256)]
         public string Username { get; set; }
 
+        [Required(ErrorMessage = "First Name is required")]
         [StringLength(160)]
+        [DisplayName("First Name")]
         public string FirstName { get; set; }
 
+        [Required(ErrorMessage = "Last Name is required")]
         [StringLength(160)]
+        [DisplayName("Last Name")]
         public string LastName { get; set; }
 
+        [Required(ErrorMessage = "Address is required")]
         [StringLength(70)]
         public string Address { get; set; }
 
+        [Required(ErrorMessage = "City is required")]
         [StringLength(40)]
         public string City { get; set; }
 
+        [Required(ErrorMessage = "State is required")]
         [StringLength(40)]
         public string State { get; set; }
 
+        [Required(ErrorMessage = "Postal Code is required")]
         [StringLength(10)]
         public string PostalCode { get; set; }
 
+        [Required(ErrorMessage = "Country is required")]
         [StringLength(40)]
         public string Country { get; set; }
 
+        [Required(ErrorMessage = "Phone is required")]
         [StringLength(24)]
         public string Phone { get; set; }
 
+        [Required(ErrorMessage = "Email Address is required")]
         [StringLength(160)]
+        [RegularExpression(@"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}",
+            ErrorMessage = "Email is is not valid.")]
+        [DataType(DataType.EmailAddress)]
+        [DisplayName("Email Address")]
         public string Email { get; set; }
 
+        [ScaffoldColumn(false)]
         [Column(TypeName = "numeric")]
         public decimal Total { get; set; }
 
