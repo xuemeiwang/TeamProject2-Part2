ab1feb47533271418a94381fa6fb9ac48a2469a4
xuemeiwang <xwang231@my.centennialcollege.ca>
2016-12-09 14:26:33 -0500

    Update shopping cart & add checkout

--- TeamProject2-Part2/App_Start/BundleConfig.cs
+++ TeamProject2-Part2/App_Start/BundleConfig.cs
@@ -23,6 +23,9 @@ namespace TeamProject2_Part2
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/app").Include(
+                "~/Scripts/app.js"));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/font-awesome.css",
